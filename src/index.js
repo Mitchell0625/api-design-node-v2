0a58bec594c22c28fb0eec5a71f2c62e2170052c
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-06-26 08:44:53 -0400

    submitting lesson 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import http from 'http'
-import { createServer } from 'http'
+import http from "http";
+import { createServer } from "http";
 
-import app from './server'
-let currentApp = app
+import app from "./server";
+let currentApp = app;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
 
 if (module.hot) {
-	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept(["./server"], () => {
+    server.removeListener("request", currentApp);
+    server.on("request", app);
+    currentApp = app;
+  });
 }
