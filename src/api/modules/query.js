b0b36a0bc4789f2b8fb97631c87dbd89fdcb7337
Alexx Mitchell <alexxmitchell25@gmail.com>
2018-07-02 14:29:14 -0400

    working on lesson-3

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -1,57 +1,76 @@
-import merge from 'lodash.merge'
-const testData = {message: 'hello'}
+import merge from "lodash.merge";
+const testData = { message: "hello" };
 
 // These are generic methods used in the generic controllers for all models
 export const controllers = {
   createOne(model, body) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   updateOne(docToUpdate, update) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   deleteOne(docToDelete) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   getOne(docToGet) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   getAll(model) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   },
 
   findByParam(model, id) {
-    return Promise.resolve(testData)
+    return Promise.resolve(testData);
   }
-}
+};
 
-export const createOne = (model) => (req, res, next) => {
+export const createOne = model => (req, res, next) => {
+  controllers
+    .createOne(model, req.body)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(err => res.status(500).send("There is an error"));
+};
 
-}
+export const updateOne = model => async (req, res, next) => {
+  controllers
+    .updateOne(docToUpdate, update)
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(500).send("There is an error"));
+};
 
-export const updateOne = (model) => async (req, res, next) => {
+export const deleteOne = model => (req, res, next) => {
+  controllers
+    .deleteOne(docToDelete)
+    .then(() => res.status(200).send("Deleted successfully"))
+    .catch(err => res.status(500).send("There is an error"));
+};
 
-}
+export const getOne = model => (req, res, next) => {
+  controllers
+    .getOne(docToGet)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).send("There is an error"));
+};
 
-export const deleteOne = (model) => (req, res, next) => {
+export const getAll = model => (req, res, next) => {
+  controllers
+    .getAll(model)
+    .then(results => res.status(200).json(results))
+    .catch(err => res.status(500).send("There is an error"));
+};
 
-}
-
-export const getOne = (model) => (req, res, next) => {
-
-}
-
-export const getAll = (model) => (req, res, next) => {
-
-}
-
-export const findByParam = (model) => (req, res, next, id) => {
-  
-}
-
+export const findByParam = model => (req, res, next, id) => {
+  controllers
+    .findByParam(model, id)
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).send("There is an error"));
+};
 
 export const generateControllers = (model, overrides = {}) => {
   const defaults = {
@@ -61,7 +80,7 @@ export const generateControllers = (model, overrides = {}) => {
     deleteOne: deleteOne(model),
     updateOne: updateOne(model),
     createOne: createOne(model)
-  }
+  };
 
-  return {...defaults, ...overrides}
-}
+  return { ...defaults, ...overrides };
+};
--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
+import express from "express";
+import setupMiddware from "./middleware";
+import { restRouter } from "./api";
+import { connect } from "./db";
+import { signin, protect } from "./api/modules/auth";
 // Declare an app from express
-const app = express()
+const app = express();
 
-setupMiddware(app)
-connect()
+setupMiddware(app);
+connect();
 // setup basic routing for index route
 
-app.use('/signin', signin)
-app.use('/api', restRouter)
+app.use("/signin", signin);
+app.use("/api", restRouter);
 // catch all
-app.all('*', (req, res) => {
-  res.json({ok: true})
-})
+app.all("*", (req, res) => {
+  res.json({ ok: true });
+});
 
-export default app
+export default app;
